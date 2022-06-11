7dffc422b967bf86be4b30bbfb4dd2cac5fcc6b8
Ilya Smirnov <iliasmirnov93@mail.ru>
2022-06-11 16:16:52 +0300

    created structure and data for radios for next.js routes api

---
+++ assets/dataInfo/dataInfo.js
@@ -0,0 +1,198 @@
+const road = '/icon/radiostations/road.png';
+ const record = '/icon/radiostations/record.png';
+const pilot = '/icon/radiostations/pilot.png';
+const europaplus = '/icon/radiostations/europaplus.png';
+const maxmum = '/icon/radiostations/maxmum.png';
+const YOFM = '/icon/radiostations/YOFM.png';
+const ELEGANT = '/icon/radiostations/ELEGANT.png';
+const ritmo = '/icon/radiostations/ritmo.png';
+const ritmoEl = '/icon/radiostations/ritmoEl.png';
+const pixel= '/icon/radiostations/pixel.png';
+const love = '/icon/radiostations/love.png';
+const dance = '/icon/radiostations/dance.png';
+const velena = '/icon/radiostations/velena.png';
+const tik_tak = '/icon/radiostations/tik-tak.png';
+const elisium = '/icon/radiostations/elisium.png';
+const elvis = '/icon/radiostations/elvis.png';
+const radio = '/icon/radiostations/radio.png';
+const shanson = '/icon/radiostations/shanson.png';
+
+export const stateInfo = {
+	radioStations: [
+		{
+			id: 1,
+			name: 'Дорожное',
+			location: 'Россия',
+			genre: "Adult Pop",
+			image: road,
+		},
+		{
+			id: 2,
+			name: 'Рекорд',
+			location: 'Россия',
+			genre: "Dance",
+			image: record,
+		},
+		{
+			id: 3,
+			name: 'Пилот',
+			location: 'Belorussia',
+			genre: "Rock",
+			image: pilot,
+		},
+		{
+			id: 4,
+			name: 'Европа Плюс',
+			location: 'Россия',
+			genre: "Power Pop",
+			image: europaplus,
+		},
+		{
+			id: 5,
+			name: 'Элвис',
+			location: 'United States',
+			genre: "Rock",
+			image: elvis,
+		},
+		{
+			id: 6,
+			name: 'Рекорд',
+			location: 'Россия',
+			genre: "Power Pop",
+			image: record,
+		},
+		{
+			id: 7,
+			name: 'Максимум',
+			location: 'Россия',
+			genre: "Pop",
+			image: maxmum,
+		},
+		{
+			id: 8,
+			name: 'Европа Плюс',
+			location: 'Россия',
+			genre: "Power Pop",
+			image: europaplus,
+		},
+		{
+			id: 9,
+			name: 'Тик-так',
+			location: 'Belorussia',
+			genre: "Adult Pop",
+			image: tik_tak,
+		},
+		{
+			id: 10,
+			name: 'Рекорд',
+			location: 'Россия',
+			genre: "Power Pop",
+			image: record,
+		},
+		{
+			id: 11,
+			name: 'Максимум',
+			location: 'Россия',
+			genre: "Pop",
+			image: maxmum,
+		},
+		{
+			id: 12,
+			name: 'Элизиум',
+			location: 'Россия',
+			genre: "Power Pop",
+			image: elisium,
+		},
+		{
+			id: 13,
+			name: 'Велена',
+			location: 'United States',
+			genre: "Adult Pop",
+			image: velena,
+		},
+		{
+			id: 14,
+			name: 'МаксимумУС',
+			location: 'Latvija',
+			genre: "Rock",
+			image: maxmum,
+		},
+		{
+			id: 15,
+			name: '101 км',
+			location: 'Россия',
+			genre: "Chanson",
+			image: shanson,
+		},
+		{
+			id: 16,
+			name: 'Yo.fm',
+			location: 'United States',
+			genre: "Electro",
+			image: YOFM,
+		},
+		{
+			id: 17,
+			name: 'Пилот',
+			location: 'Belorussia',
+			genre: "Rock",
+			image: pilot,
+		},
+		{
+			id: 18,
+			name: 'Ritmo',
+			location: 'Germany',
+			genre: "Hip-hop",
+			image: ritmo,
+		},
+		{
+			id: 19,
+			name: 'Элегант',
+			location: 'Latviya',
+			genre: "Chanson",
+			image: ELEGANT,
+		},
+		{
+			id: 20,
+			name: 'RitmoEl',
+			location: 'Germany',
+			genre: "Electro",
+			image: ritmoEl,
+		},
+		{
+			id: 21,
+			name: 'Европа Плюс',
+			location: 'Россия',
+			genre: "Power Pop",
+			image: europaplus,
+		},
+		{
+			id: 22,
+			name: 'Pixel',
+			location: 'USA',
+			genre: "r&b soul",
+			image: pixel,
+		},
+		{
+			id: 23,
+			name: 'Love',
+			location: 'USA',
+			genre: "Pop",
+			image: love,
+		},
+		{
+			id: 24,
+			name: 'Radio',
+			location: 'Germany',
+			genre: "Adult Pop",
+			image: radio,
+		},
+		{
+			id: 25,
+			name: 'DanceFM',
+			location: 'United States',
+			genre: "Dance",
+			image: dance,
+		},
+	]
+};
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Home({ data }) {
+  const element = data
+    .map(c =>
+      <Image key={c.id} src={c.image} width="64" height="64"  />
+    );
+    
   return (
     <div>
       Привет, мир!
+      {element}
     </div>
-  )
-}
+  );
+};
+
+Home.getInitialProps = async () => {
+  const response = await fetch('http://localhost:3000/api/hello', {
+    method: 'GET',
+  });
+  const data = await response.json();
+  return {
+    data
+  }
+};
